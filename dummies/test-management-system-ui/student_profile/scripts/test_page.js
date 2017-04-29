1a385ce0e6c50464c0f2b74570487fab820e990a
Amrit Jha <amritjha107@gmail.com>
2017-04-30 01:12:14 +0530

    Update test_page.js

--- dummies/test-management-system-ui/student_profile/scripts/test_page.js
+++ dummies/test-management-system-ui/student_profile/scripts/test_page.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
     $(".bm i").click(function(){
         $(this).toggleClass("myclass");
     });
+    $("#next-five").click(showNextFiveQs);
+    
 });
-
+function showNextFiveQs(){
+    var question=6;
+    for(var i=1; i<question;i++){
+        $(".random-class").append('<div class="questions"> Q1. <span class="bm"><i class="fa fa-bookmark " id="bm"></i></span><br><input type="radio" name="option" > a) <br> <input type="radio" name="option" > b) <br> <input type="radio" name="option"> c) <br><input type="radio" name="option"> d) <button type="reset" id="clear">CLEAR</button></div>');
+        $(".another-random-class").append('<div class="two columns color">1</div>');
+    }
+}
 /*$("#clear").click(function(){
     $("input[radio]").removeAttr("checked");
 });*/
@@ -12,20 +20,6 @@ $("#clear").click(function()
     document.getElementById("#input").checked = false;
 });
 
-/*var question=5;
-for(var i=1;i<question;i++){
-    $(".questions").append('<div class="questions">
-                        
-                        Q1. <span class="bm"><i class="fa fa-bookmark " id="bm"></i></span></br>
-                        
-                        <input type="radio" name="option" > a) <br>
-                        <input type="radio" name="option" > b) <br>
-                        <input type="radio" name="option"> c) <br>
-                        <input type="radio" name="option"> d) 
-                        <button type="reset" id="clear">CLEAR</button>
-                       
-                        
 
-                    </div>');
-}*/
+
 
