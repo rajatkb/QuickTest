58758be5bf50ddf010b3ab600373e6da156a49d7
Amrit Jha <amritjha107@gmail.com>
2017-04-30 22:04:35 +0530

    Update dashboard_teacher_scripts.js

--- dummies/teachers-and-login/scripts/dashboard_teacher_scripts.js
+++ dummies/teachers-and-login/scripts/dashboard_teacher_scripts.js
@@ -10,12 +10,14 @@ function onLoading(){
         var new_qs_index = questions.length+1;
         addNewQuestion(new_qs_index);
     });
+    $(".escape-form-button").click(escapeFormWindow);
 }
 function updateTeachersInfo(){
     $(".grand-container").fadeIn(500);
     var pageHeight = String(parseFloat($(".main-container").css("height"))+150);
     $(".grand-container").css("height", pageHeight);
     $(".update-info-form").fadeIn(500);
+    updateInfoFormSubmission();
 }
 function startTest(){
     $(".grand-container").fadeIn(500);
@@ -24,9 +26,10 @@ function startTest(){
     $(".ongoing-test-info").fadeIn(500);
 }
 function addNewQuestion(index){
-    var newQuestionTemplate = '<div class="question" id="question'+ index + '"' + '><input class="form-item" name="newtest[qs_' + index + ']" placeholder="Enter question" /><div class="row"><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_a]" placeholder="A" /></div><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_b]" placeholder="B" /></div></div><div class="row"><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_c]" placeholder="C" /></div><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_d]" placeholder="D" /></div></div><div class="row"><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_answer]" placeholder="Correct choice" /></div><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_marks]" placeholder="Marks for this question" /></div></div>'
-    $('.more-qs-container').append(newQuestionTemplate);
-    $('#question'+String(index-1)).hide('slide', {direction: 'left'}, 500);
+    var newQuestionTemplate = '<div class="question" id="question'+ index + '"' + '><input class="form-item qs_title" name="newtest[qs_' + index + ']" placeholder="Enter question '+ index +'" /><div class="row"><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_a]" placeholder="A" /></div><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_b]" placeholder="B" /></div></div><div class="row"><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_c]" placeholder="C" /></div><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_choice_d]" placeholder="D" /></div></div><div class="row"><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_answer]" placeholder="Correct choice" /></div><div class="six columns"><input class="form-item" name="newtest[qs_' + index + '_marks]" placeholder="Marks for this question" /></div></div>'
+    $('#question'+String(index-1)).hide('slide', {direction: 'left'}, 300, function(){
+        $('.more-qs-container').append(newQuestionTemplate);
+    });
 } 
 function fixButtons(){
     $(".test-submitted .edit-test").css({"opacity": "0.5", "box-shadow": "none"});
@@ -47,14 +50,8 @@ function loadNewTestForm(){
     $(".dummy-grand").css("height", formHeight);
     $(".grand-container").css("height", formHeight);
 }
-/*
-
-<script
-  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-  crossorigin="anonymous"></script>
-  <script
-  src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
-  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
-  crossorigin="anonymous"></script>
-*/
+function escapeFormWindow(){
+    $(".grand-container").fadeOut(50, function(){
+        $(".dummy-grand").hide("fast");
+    });
+}
