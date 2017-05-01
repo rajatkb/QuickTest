7bb8180b3c5f479dec7da62f2cf8122bea4c7794
Amrit Jha <amritjha107@gmail.com>
2017-05-02 00:53:50 +0530

    Update nav.js

--- dummies/teachers-and-login-2.0(updated) - Amrit/scripts/nav.js
+++ dummies/teachers-and-login-2.0(updated) - Amrit/scripts/nav.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
     $('.toggle').click(function () {
-        console.log("hi");
-
         $('nav ul').slideToggle();
     });
     
     
     $(window).resize(function () {
-        
-        if ($(window).width() > 700 ) {
+        if ($(window).width() > 700) {
             $('nav ul').removeAttr('style');
         }
     });
