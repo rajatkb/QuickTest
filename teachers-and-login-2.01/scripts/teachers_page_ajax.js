6cab950771d1d95295bf566cf3cc7348fb283e0a
Amrit Jha <amritjha107@gmail.com>
2017-05-04 08:41:35 +0530

    Update teachers_page_ajax.js

--- teachers-and-login-2.01/scripts/teachers_page_ajax.js
+++ teachers-and-login-2.01/scripts/teachers_page_ajax.js
@@ -1,22 +1,6 @@
 $("document").ready(function(){
-    $(".del-qs-button").click(deleteQues);
+    $(".delele-test").click(deleteQues);
 });
-function deleteQues(){
-    var screen_qs_index = screenQuestionIndex();
-    $("#question"+screen_qs_index).hide("fast", function(){
-        $("#question"+screen_qs_index).remove();
-    });
-    $.ajax({
-        type: "POST",
-        data: screen_qs_index,
-        dataType: "text", 
-        statusCode: {
-            404: function(){
-                console.log("Error: 404");
-            }    
-        }
-    })
-    .done(function(data){
-        console.log(data);
-    });
+function deleteTest(){
+   
 }
